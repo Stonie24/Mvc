699cdc5d5ec7149d8a34137666e8018469b09809
Stonie24 <willefille24@gmail.com>
2024-04-09 10:27:26 +0200

    liten fix

--- app/src/Controller/LuckyControllerJson.php
+++ app/src/Controller/LuckyControllerJson.php
@@ -8,14 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyControllerJson
 {
-    #[Route("/api/lucky/number")]
-    public function jsonNumber(): Response
+
+    #[Route("/api")]
+    public function jsonRoutes(): Response
     {
         $number = random_int(0, 100);
 
         $data = [
-            'lucky-number' => $number,
-            'lucky-message' => 'Hi there!',
+            "app_lucky_hi"  => " ANY      ANY      ANY    /lucky/hi",
+            "lucky" => "ANY      ANY      ANY    /lucky",
+            "home" => "ANY      ANY      ANY    /",
+            "about" => "ANY      ANY      ANY    /about",
+            "report" => "ANY      ANY      ANY    /report/{kmom}"
+        ];
+
+        
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        $quote = random_int(0, 2);
+
+        $list = [
+            "Det var en gång en gång och den var sandad",
+            "Här kommer göran göran ade göran",
+            "här har en hare hoppat hare? a det hare"
+        ];
+
+        $data = [
+            'quote' => $list[$quote],
         ];
 
         

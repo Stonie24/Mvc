d9de8a12332adf5b62ff202851f4343d4ec2c09a
Stonie24 <willefille24@gmail.com>
2026-06-04 12:42:37 +0200

    Added more css for the thingy aka game

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Game\BlackjackGame;
 use App\Entity\Player;
 use App\Repository\PlayerRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -62,5 +63,95 @@ class ProjController extends AbstractController
     public function game(): Response
     {
         return $this->render('proj/game_landing.html.twig');
+    }
+
+    #[Route('/proj/game/play', name: 'proj_game_play', methods: ['GET'])]
+    public function play(
+        SessionInterface $session,
+        PlayerRepository $playerRepository
+    ): Response {
+        $playerId = $session->get('player_id');
+        $player = $playerRepository->find($playerId);
+        $numHands = $session->get('num_hands', 1);
+        $bet = $session->get('bet', 50);
+
+        $game = new BlackjackGame($numHands);
+        $game->deal();
+        $session->set('proj_game', $game);
+
+        return $this->render('proj/game.html.twig', [
+            'game' => $game,
+            'player' => $player,
+            'bet' => $bet,
+            'dealerHand' => [$game->getDealerHand()->getString()[0], '🂠'],
+            'dealerScore' => $game->getDealerVisibleScore(),
+        ]);
+    }
+
+    #[Route('/proj/game/hit', name: 'proj_hit', methods: ['POST'])]
+    public function projHit(
+        SessionInterface $session,
+        PlayerRepository $playerRepository
+    ): Response {
+        $game = $session->get('proj_game');
+        $game->hit();
+        $session->set('proj_game', $game);
+
+        $player = $playerRepository->find($session->get('player_id'));
+        $bet = $session->get('bet', 50);
+        $roundOver = $game->isRoundOver();
+
+        return $this->render('proj/game.html.twig', [
+            'game' => $game,
+            'player' => $player,
+            'bet' => $bet,
+            'dealerHand' => $roundOver
+                ? $game->getDealerHand()->getString()
+                : [$game->getDealerHand()->getString()[0], '🂠'],
+            'dealerScore' => $roundOver
+                ? $game->calculateScore($game->getDealerHand())
+                : $game->getDealerVisibleScore(),
+        ]);
+    }
+
+    #[Route('/proj/game/stand', name: 'proj_stand', methods: ['POST'])]
+    public function projStand(
+        SessionInterface $session,
+        PlayerRepository $playerRepository,
+        EntityManagerInterface $em
+    ): Response {
+        $game = $session->get('proj_game');
+        $game->stand();
+        $session->set('proj_game', $game);
+
+        $player = $playerRepository->find($session->get('player_id'));
+        $bet = $session->get('bet', 50);
+        $roundOver = $game->isRoundOver();
+
+        // Uppdatera balansen när rundan är över
+        if ($roundOver) {
+            $numHands = $game->getNumHands();
+            for ($i = 0; $i < $numHands; $i++) {
+                $status = $game->getHandStatus($i);
+                if (in_array($status, ['player_win', 'dealer_bust'])) {
+                    $player->setBalance($player->getBalance() + $bet);
+                } elseif (in_array($status, ['player_bust', 'player_lost'])) {
+                    $player->setBalance($player->getBalance() - $bet);
+                }
+            }
+            $em->flush();
+        }
+
+        return $this->render('proj/game.html.twig', [
+            'game' => $game,
+            'player' => $player,
+            'bet' => $bet,
+            'dealerHand' => $roundOver
+                ? $game->getDealerHand()->getString()
+                : [$game->getDealerHand()->getString()[0], '🂠'],
+            'dealerScore' => $roundOver
+                ? $game->calculateScore($game->getDealerHand())
+                : $game->getDealerVisibleScore(),
+        ]);
     }
 }
